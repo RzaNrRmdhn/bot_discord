ac1d2b36d9fc107704dd2456fcd17bb1f513a9da
Reza Nur Ramadhan <rezanurramadhan292@gmail.com>
2025-07-14 22:43:14 +0700

    update

--- events/onboarding.js
+++ events/onboarding.js
@@ -4,7 +4,17 @@ export default async function handleOnboarding(member) {
     const welcomeChannel = member.guild.channels.cache.get('1370350795781967922');
 
     if(welcomeChannel) {
-        await welcomeChannel.send(`👋Haloow Welkam!! Salken Aku Nevas, <@${member.id}>! 
-            Silakan kenalan di <#1392801305424171008> dan Kalau ambil role di <#1340643587041398876>! Moga Betah yawwww []~(￣▽￣)~*`);
+
+        const hardcodedMessage = `
+### 👋Haloow Welkam!! <@${member.id}>!
+============================================
+Salken Aku Nevas (≧∇≦)!
+- Silakan kenalan di <#1392801305424171008>
+- Kalau ambil role di <#1340643587041398876>!
+=============================================
+Moga Betah yawwww []~(￣▽￣)~*
+`;
+
+        await welcomeChannel.send({ content: hardcodedMessage });
     }
 }
